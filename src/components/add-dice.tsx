e3815da02e9133ec34732a70eff3e0f5cc6941b6
ImDarkly <svit.darkly@gmail.com>
2024-07-27 17:40:02 +0300

    style: drawer changed to dialog on desktop

--- src/components/add-dice.tsx
+++ src/components/add-dice.tsx
@@ -1,6 +1,16 @@
-import { useState, useEffect } from "react";
+// AddDice.tsx
+
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { Button } from "./ui/button";
 import { Icon } from "@iconify/react";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
 import {
     Drawer,
     DrawerClose,
@@ -14,11 +24,25 @@ import {
 import { Input } from "./ui/input";
 import { toast } from "sonner";
 import useDiceStore from "@/zustand/store";
+import useMediaQuery from "@/hooks/useMediaQuery";
+
+interface AddDiceProps {
+    open: boolean;
+    onOpenChange: Dispatch<SetStateAction<boolean>>;
+    minValue: string;
+    maxValue: string;
+    isButtonDisabled: boolean;
+    setMinValue: Dispatch<SetStateAction<string>>;
+    setMaxValue: Dispatch<SetStateAction<string>>;
+    handleAdd: () => void;
+}
 
 const AddDice = () => {
     const [minValue, setMinValue] = useState("");
     const [maxValue, setMaxValue] = useState("");
     const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+    const [open, setOpen] = useState(false);
+    const isDesktop = useMediaQuery("(min-width: 768px)");
 
     const setDiceRange = useDiceStore((state) => state.setDiceRange);
 
@@ -45,46 +69,122 @@ const AddDice = () => {
         setMaxValue("");
     };
 
-    return (
-        <Drawer>
-            <DrawerTrigger>
-                <Button size={"icon"} variant={"outline"} className="size-16">
-                    <Icon icon="heroicons:plus-16-solid" />
-                </Button>
-            </DrawerTrigger>
-            <DrawerContent>
-                <DrawerHeader>
-                    <DrawerTitle className="text-left">
-                        Add new dice
-                    </DrawerTitle>
-                    <DrawerDescription className="text-left">
-                        Please choose the range.
-                    </DrawerDescription>
-                </DrawerHeader>
-                <div className="flex flex-row gap-2 px-4">
-                    <Input
-                        placeholder="Minimal value"
-                        autoFocus
-                        value={minValue}
-                        onChange={(e) => setMinValue(e.target.value)}
-                    />
-                    <Input
-                        placeholder="Maximum value"
-                        value={maxValue}
-                        onChange={(e) => setMaxValue(e.target.value)}
-                    />
-                </div>
-                <DrawerFooter>
-                    <Button onClick={handleAdd} disabled={isButtonDisabled}>
-                        Add
-                    </Button>
-                    <DrawerClose>
-                        <Button variant="ghost">Cancel</Button>
-                    </DrawerClose>
-                </DrawerFooter>
-            </DrawerContent>
-        </Drawer>
+    return isDesktop ? (
+        <AddDiceDialog
+            open={open}
+            onOpenChange={setOpen}
+            minValue={minValue}
+            maxValue={maxValue}
+            isButtonDisabled={isButtonDisabled}
+            setMinValue={setMinValue}
+            setMaxValue={setMaxValue}
+            handleAdd={handleAdd}
+        />
+    ) : (
+        <AddDiceDrawer
+            open={open}
+            onOpenChange={setOpen}
+            minValue={minValue}
+            maxValue={maxValue}
+            isButtonDisabled={isButtonDisabled}
+            setMinValue={setMinValue}
+            setMaxValue={setMaxValue}
+            handleAdd={handleAdd}
+        />
     );
 };
+
+const AddDiceDialog = ({
+    open,
+    onOpenChange,
+    minValue,
+    maxValue,
+    isButtonDisabled,
+    setMinValue,
+    setMaxValue,
+    handleAdd,
+}: AddDiceProps) => (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+        <DialogTrigger asChild>
+            <Button size={"icon"} variant={"outline"} className="size-16">
+                <Icon icon="heroicons:plus-16-solid" />
+            </Button>
+        </DialogTrigger>
+        <DialogContent>
+            <DialogHeader>
+                <DialogTitle className="text-left">Add new dice</DialogTitle>
+                <DialogDescription className="text-left">
+                    Please choose the range.
+                </DialogDescription>
+            </DialogHeader>
+            <div className="flex flex-row gap-2">
+                <Input
+                    placeholder="Minimal value"
+                    autoFocus
+                    value={minValue}
+                    onChange={(e) => setMinValue(e.target.value)}
+                />
+                <Input
+                    placeholder="Maximum value"
+                    value={maxValue}
+                    onChange={(e) => setMaxValue(e.target.value)}
+                />
+            </div>
+            <div className="flex justify-end mt-4">
+                <Button onClick={handleAdd} disabled={isButtonDisabled}>
+                    Add
+                </Button>
+            </div>
+        </DialogContent>
+    </Dialog>
+);
+
+const AddDiceDrawer = ({
+    open,
+    onOpenChange,
+    minValue,
+    maxValue,
+    isButtonDisabled,
+    setMinValue,
+    setMaxValue,
+    handleAdd,
+}: AddDiceProps) => (
+    <Drawer open={open} onOpenChange={onOpenChange}>
+        <DrawerTrigger asChild>
+            <Button size={"icon"} variant={"outline"} className="size-16">
+                <Icon icon="heroicons:plus-16-solid" />
+            </Button>
+        </DrawerTrigger>
+        <DrawerContent>
+            <DrawerHeader>
+                <DrawerTitle className="text-left">Add new dice</DrawerTitle>
+                <DrawerDescription className="text-left">
+                    Please choose the range.
+                </DrawerDescription>
+            </DrawerHeader>
+            <div className="flex flex-row gap-2 px-4">
+                <Input
+                    placeholder="Minimal value"
+                    autoFocus
+                    value={minValue}
+                    onChange={(e) => setMinValue(e.target.value)}
+                />
+                <Input
+                    placeholder="Maximum value"
+                    value={maxValue}
+                    onChange={(e) => setMaxValue(e.target.value)}
+                />
+            </div>
+            <DrawerFooter>
+                <Button onClick={handleAdd} disabled={isButtonDisabled}>
+                    Add
+                </Button>
+                <DrawerClose>
+                    <Button variant="ghost">Cancel</Button>
+                </DrawerClose>
+            </DrawerFooter>
+        </DrawerContent>
+    </Drawer>
+);
 
 export default AddDice;
