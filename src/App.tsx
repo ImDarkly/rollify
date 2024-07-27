63d4bb5a904330903df7f8233e1fbcfe339bd682
ImDarkly <svit.darkly@gmail.com>
2024-07-27 22:59:02 +0300

    style: added motion

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Dice from "./components/dice";
 import useDiceStore from "@/zustand/store";
 import { ThemeProvider } from "@/components/theme-provider";
 import TopBar from "./components/top-bar";
+import { AnimatePresence, motion } from "framer-motion";
 
 function App() {
     const dice = useDiceStore((state) => state.dice);
@@ -14,20 +15,38 @@ function App() {
             <div className="h-svh flex flex-col items-center">
                 <TopBar />
                 <div className="flex-grow flex flex-wrap justify-center items-center content-center gap-2 overflow-auto max-w-screen-lg p-4">
-                    {Object.keys(dice).map((key) => {
-                        const diceId = parseInt(key, 10); // Convert key to a number
-                        const { min, max, value } = dice[diceId];
-                        return (
-                            <Dice
-                                key={diceId}
-                                id={diceId}
-                                min={min}
-                                max={max}
-                                value={value}
-                            />
-                        );
-                    })}
-                    <AddDice />
+                    <AnimatePresence>
+                        {Object.keys(dice).map((key) => {
+                            const diceId = parseInt(key, 10); // Convert key to a number
+                            const { min, max, value } = dice[diceId];
+                            return (
+                                <motion.div
+                                    key={diceId}
+                                    layout
+                                    initial={{ opacity: 0, scale: 0.8 }}
+                                    animate={{ opacity: 1, scale: 1 }}
+                                    exit={{ opacity: 0, scale: 0.8 }}
+                                    transition={{ duration: 0.1 }}
+                                >
+                                    <Dice
+                                        id={diceId}
+                                        min={min}
+                                        max={max}
+                                        value={value}
+                                    />
+                                </motion.div>
+                            );
+                        })}
+                        <motion.div
+                            layout
+                            initial={{ opacity: 0, scale: 0.8 }}
+                            animate={{ opacity: 1, scale: 1 }}
+                            exit={{ opacity: 0, scale: 0.8 }}
+                            transition={{ duration: 0.1 }}
+                        >
+                            <AddDice />
+                        </motion.div>
+                    </AnimatePresence>
                 </div>
                 <BottomBar />
             </div>
--- src/components/dice.tsx
+++ src/components/dice.tsx
@@ -12,14 +12,17 @@ const Dice = ({ id, min, max, value }: DiceProps) => {
     const removeDice = useDiceStore((state) => state.removeDice);
 
     return (
-        <div onClick={() => removeDice(id)} className="dice-container">
-            <Button size={"icon"} variant={"secondary"} className="size-16">
-                <div>
-                    <p className="text-xs ">{`${min}-${max}`}</p>
-                    <p className="text-xl font-bold">{value}</p>
-                </div>
-            </Button>
-        </div>
+        <Button
+            onClick={() => removeDice(id)}
+            size={"icon"}
+            variant={"secondary"}
+            className="size-16"
+        >
+            <div>
+                <p className="text-xs ">{`${min}-${max}`}</p>
+                <p className="text-xl font-bold">{value}</p>
+            </div>
+        </Button>
     );
 };
 
