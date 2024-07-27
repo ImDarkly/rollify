30ad280079843c56aa07d5647187c960d87a93b3
ImDarkly <svit.darkly@gmail.com>
2024-07-27 20:40:52 +0300

    style: fixed theme switch

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -2,7 +2,7 @@ import { ModeToggle } from "./mode-toggle";
 
 const TopBar = () => {
     return (
-        <div className="w-full h-24 flex flex-row justify-center items-center">
+        <div className="w-full h-24 flex flex-row justify-center absolute items-center">
             <ModeToggle />
         </div>
     );
